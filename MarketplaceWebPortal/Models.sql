03e2f882631d627cbea0cbff956d7a71828b48d1
HanxuanDai <andrew.dai.hx@gmail.com>
2021-02-15 21:54:12 -0600

    delete duplicated columns in [Models] table

--- MarketplaceWebPortal/Models.sql
+++ MarketplaceWebPortal/Models.sql
@@ -4,9 +4,6 @@
     [model_name] NVARCHAR(50) NULL,
     [model_year]     INT NOT NULL,
     PRIMARY KEY CLUSTERED ([Model_id] ASC),
-    [use_type] NVARCHAR(50) NULL,
-    [application] NVARCHAR(50) NULL,
-    [mounting_location] NVARCHAR(50) NULL,
-    [accessories] NVARCHAR(50) NULL,
+
     
 )
