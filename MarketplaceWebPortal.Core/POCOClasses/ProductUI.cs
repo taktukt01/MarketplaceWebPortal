3c5f0a4506931d3e8fe60cf08ed3f3a6a0959fb8
HanxuanDai <andrew.dai.hx@gmail.com>
2021-02-16 06:16:55 -0600

    Create .Core layer, using POCO classes. In this step, unbind relationship between Entity and Model

---
+++ MarketplaceWebPortal.Core/POCOClasses/ProductUI.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace MarketplaceWebPortal.Core.POCOClasses
+{
+    public class ProductUI
+    {
+        public int ProductID { get; set; }
+        public Nullable<int> ModelID { get; set; }
+        public Nullable<int> ManufacturerID { get; set; }
+        public string ProductImgUrl { get; set; }
+        public Nullable<int> SubCategoryID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
--- MarketplaceWebPortal.EntityLayer/Repository/ProductRepository.cs
+++ MarketplaceWebPortal.EntityLayer/Repository/ProductRepository.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using MarketplaceWebPortal.EntityLayer.Entities;
+using MarketplaceWebPortal.Core.POCOClasses;
 
 namespace MarketplaceWebPortal.EntityLayer.Repository
 {
     public class ProductRepository
     {
         MarketplaceWebPortal.EntityLayer.Entities.Entities _entities;
-        public List<Product> getProductDetails()
+        public List<ProductUI> getProductDetails()
         {
-            List<Product> pList = new List<Product> 
+            List<ProductUI> pListUI = new List<ProductUI> 
             {
-                new Product{ProductID=1,ModelID=1,ManufacturerID=1,ProductImgUrl="/p.jpg",SubCategoryID=1,ProductName="Product1"},
-                new Product{ProductID=2,ModelID=21,ManufacturerID=11,ProductImgUrl="/p1.jpg",SubCategoryID=1,ProductName="Product2"},
-                new Product{ProductID=3,ModelID=33,ManufacturerID=10,ProductImgUrl="/p111.jpg",SubCategoryID=1,ProductName="Product133"}
+                new ProductUI{ProductID=1,ModelID=1,ManufacturerID=1,ProductImgUrl="/p.jpg",SubCategoryID=1,ProductName="Product1"},
+                new ProductUI{ProductID=2,ModelID=21,ManufacturerID=11,ProductImgUrl="/p1.jpg",SubCategoryID=1,ProductName="Product2"},
+                new ProductUI{ProductID=3,ModelID=33,ManufacturerID=10,ProductImgUrl="/p111.jpg",SubCategoryID=1,ProductName="Product133"}
 
             };
-            return pList;
+            return pListUI;
            // return _entities.Products.ToList();
         }
     }
