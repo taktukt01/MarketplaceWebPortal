68144c59d1ea6a3cb00a1bf8a9f6c2d9faac904a
Lulu0325 <78986880+Lulu0325@users.noreply.github.com>
2021-02-16 21:24:05 -0500

    Add files via upload
    SelectionPage is the cshtml View Page.
    Logo. png needs to be put in a new file(Create an Image file in UI project and put Logo.png in it)
    (SelectionPageController.cs is the controller)

---
+++ SelectionPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarketplaceWebPortal.Controllers
+{
+    public class SelectionController : Controller
+    {
+        public ActionResult Selection()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Details(string selectedtText, string Category)
+        {
+            return View("Selection");
+        }
+    }
+}
