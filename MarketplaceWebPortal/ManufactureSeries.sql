933f85a007aa5f2ef87a30b2641dffeacc1c56a1
Taktuk <taktukta@buffalo.edu>
2021-02-18 14:45:00 -0500

    updated edmx

--- MarketplaceWebPortal/ManufactureSeries.sql
+++ MarketplaceWebPortal/ManufactureSeries.sql
@@ -4,7 +4,7 @@
 	[ManufactureID] INT NOT NULL,
 	[SeriesID] INT NULL, 
 	PRIMARY KEY CLUSTERED ([m_s_id] ASC),
-	CONSTRAINT [FK_dbo.ManufactureSeries_dbo.Series_CourseID] FOREIGN KEY ([SeriesID]) 
+	CONSTRAINT [FK_dbo.ManufactureSeries_dbo.Series_SeriesID] FOREIGN KEY ([SeriesID]) 
         REFERENCES [dbo].[Series] ([SeriesID]) ON DELETE CASCADE,
 	CONSTRAINT [FK_dbo.ManufactureSeries_dbo.Manufactures_ManufactureID] FOREIGN KEY ([ManufactureID]) 
         REFERENCES [dbo].[Manufactures] ([ManufactureID]) ON DELETE CASCADE
